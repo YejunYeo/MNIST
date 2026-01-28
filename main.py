95b74bb5e4799740e6d5ef20159393e5f50e13d4
YejunYeo <yjyeo2006@gmail.com>
2026-01-27 23:01:02 -0700

    Tried to implement forward pass. Just read a little bit tbh

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import random
 import numpy as np
+from torchvision import datasets, transforms
 
+
+# weight are assigned random floats
 weights = np.random.rand(10,784)
 
+
+mnist = datasets.MNIST(root =" ./data", download = True, train = True )
+
+image,label = mnist[0]
+
+print (label)
+
+# forward pass
+
+# alright I asked Claude why I have to use  totensor instead of  PILToTensor it said smth abt gradient. Get back to it after u code the gradient part.
+
+for image,label in mnist:
 
 
