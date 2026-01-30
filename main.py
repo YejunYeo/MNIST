afacb13a6aa4862130367a3ac8cc4d234617a8b6
YejunYeo <yjyeo2006@gmail.com>
2026-01-29 22:51:37 -0700

    implemented Forward Pass

--- main.py
+++ main.py
@@ -10,22 +10,27 @@ weights = torch.rand(10,784)
 mnist = datasets.MNIST(root =" ./data", download = True, train = True )
 
 image,label = mnist[0]
-
-print (label)
-
 # forward pass
 
 # alright I asked Claude why I have to use  totensor instead of  PILToTensor it said smth abt gradient. Get back to it after u code the gradient part.
 
 to_tensor = transforms.ToTensor()
 
+
 for image,label in mnist:
     img_to_tensor = to_tensor(image)
     flattened_tensor = torch.flatten(img_to_tensor)
-    for i in range 10:
-        max = -9999
-        scores =  torch.dot(flattened_tensor, weights[i])
-        if scores > max:
+    # maximum = -999
+    num_scores = np.zeros(10)
 
-    
+    # a number is assigned for each possible digit from 0 to 9
+    for i in range (10):
+        score  =  torch.dot(flattened_tensor, weights[i])
+        num_scores[i] = score
+
+    # in forward pass,a prediction is made using initial weights
+    print (np.argmax(num_scores))
+
+print ("Hello WOrld")
+
 
